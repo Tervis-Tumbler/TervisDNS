b8dc84b2ebd80f27221856b46c9ca79b54a45f29
ChrisMagnuson <hourglasssand@gmail.com>
2016-08-29 13:24:25 -0400

    Initial Commit

---
+++ TervisDNS.psm1
@@ -0,0 +1,5 @@
+﻿function Get-TervisDNSMXMailServer {
+    Resolve-DnsName -Name tervis.com -Type MX -Server 8.8.8.8 | 
+    sort prefernce -Descending | 
+    select -First 1 -ExpandProperty NameExchange
+}
