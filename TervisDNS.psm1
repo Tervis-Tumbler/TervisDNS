d45506e727ace6936a301cde844029cba9828377
mastermohl <mastermohl@gmail.com>
2017-04-26 11:39:35 -0400

    Revert "+Remove-TervisDNSRecordsforVM"
    This reverts commit c6cb4a31eb7065bc37e6888d46dbb088cd4f3461.

--- TervisDNS.psm1
+++ TervisDNS.psm1
@@ -3,21 +3,3 @@
     sort prefernce -Descending | 
     select -First 1 -ExpandProperty NameExchange
 }
-
-function Remove-TervisDNSRecordsforVM{
-    [CmdletBinding()]
-    param(
-        [parameter(Mandatory, ValueFromPipeline)]$VM,
-        [switch]$PassThru
-    )
-    $NodeToDelete = $VM.Name
-    $DNSServer = "inf-dc1"
-    $ZoneName = "tervis.prv"
-    $NodeDNS = $null
-    $NodeDNSARecord = Get-DnsServerResourceRecord -ZoneName $ZoneName -ComputerName $DNSServer -Node $NodeToDelete -RRType A -ErrorAction SilentlyContinue
-    $NodeDNSCnameRecord = Get-DnsServerResourceRecord -ZoneName $ZoneName -ComputerName $DNSServer -RRType CName -ErrorAction SilentlyContinue | where { $_.recorddata.hostnamealias -Match $nodetodelete}
-    if($NodeDNSARecord){Remove-DnsServerResourceRecord -ZoneName $ZoneName -ComputerName $DNSServer -InputObject $NodeDNSARecord -Force}
-    if($NodeDNSCnameRecord){Remove-DnsServerResourceRecord -ZoneName $ZoneName -ComputerName $DNSServer -InputObject $NodeDNSCnameRecord -Force}
-
-    if($PassThru) {$VM}
-}
